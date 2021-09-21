57a2d161ba408a52c7d029d6e5292c1fae2b9847
mironmiron3 <miroslav.r.ivanchev@gmail.com>
2021-09-21 16:52:29 +0300

    Adding functionality

--- main.cpp
+++ main.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 
 int main() {
-    cout << "Hello I am Miroslav"<< endl;
+    int number;
+    do{
+        cout << "Enter a number between 1 and 10:";
+        cin >> number;
+    } while (number < 1 || n > 10);
+    for (int i = 0; i < number; i ++){
+        for (int j = 0; j < number; j ++){
+            cout << " * ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
