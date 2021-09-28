42d115bf2d6dff4c21e75058f1c34bdd4b78b3f1
mironmiron3 <miroslav.r.ivanchev@gmail.com>
2021-09-28 15:27:12 +0300

    Adding functionality

--- main.cpp
+++ main.cpp
@@ -2,13 +2,24 @@
 #include <iomanip>
 #include <cmath>
 using namespace std;
+double getPerimeter(int a, int b); // declaring it so that it knows about it in main function!
+double getArea(int a, int b);
 
 int main() {
-    int a, b;
+    float a, b;
     cin >> a >> b;
-    float hyp;
-    hyp = sqrt((pow(a, 2)) + pow(b, 2));
-    cout << hyp << endl;
-
+    cout << getPerimeter(a ,b) << setw(3) << getArea(a, b) << endl;
     return 0;
+
+
+}
+double getPerimeter(int a, int b) {
+    float perimeter = 2 * a + 2 * b;
+    return perimeter;
+
+
+}
+double getArea(int a, int b){
+       float area = a * b;
+       return area;
 }
