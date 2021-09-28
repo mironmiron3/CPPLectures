2b49a98f82fc2a247efb8dad156f8e5e8164d062
mironmiron3 <miroslav.r.ivanchev@gmail.com>
2021-09-28 14:50:58 +0300

    Adding functionality

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <iomanip>
-
+#include <cmath>
 using namespace std;
 
 int main() {
-    for (int i = 10; i < 35; i += 5) {
-        for (int j = 4; j > 0; j--) {
-            cout << j << endl;
-        }
-    }
+    int a, b;
+    cin >> a >> b;
+    float hyp;
+    hyp = sqrt((pow(a, 2)) + pow(b, 2));
+    cout << hyp << endl;
 
     return 0;
 }
